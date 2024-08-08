b22d82ed675d5399d8919330c8bffeb8e8cf5d63
rwkliu <114320485+rwkliu@users.noreply.github.com>
2024-08-08 01:43:58 -0700

    Add starter test code for my_malloc

--- src/main.c
+++ src/main.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
+#include <string.h>
 #include "my_malloc.h"
 
 int main() {
+    char *src = "hello";
+    char *dest = my_malloc(strlen(src) + 1);
+    
+    strcpy(dest, src);
+    printf("%s\n", dest);
     return 0;
 }
