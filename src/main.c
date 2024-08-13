d9b470cf0e8ddd96c90122e0040ee1f013b1e410
Ricky Liu <rliu@blacksky.com>
2024-08-13 09:41:22 -0700

    Add my_calloc function call

--- src/main.c
+++ src/main.c
@@ -7,10 +7,14 @@ int main() {
     char *dest = my_malloc(strlen(src) + 1);
     char *src2 = "goodbye";
     char *dest2 = my_malloc(strlen(src2) + 1);
+    char *src3 = "world";
+    char *dest3 = my_calloc(1, strlen(src3) + 1);
     
     strcpy(dest, src);
     strcpy(dest2, src2);
+    strcpy(dest3, src3);
     printf("dest: %s\n", dest);
     printf("dest2: %s\n", dest2);
+    printf("dest3: %s\n", dest3);
     return 0;
 }
