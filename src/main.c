9a2154ca702f8a67ae30504e96dcf4f56f7d43a1
Ricky Liu <rliu@blacksky.com>
2024-08-13 13:49:20 -0700

    Rearrange tests

--- src/main.c
+++ src/main.c
@@ -5,16 +5,18 @@
 int main() {
     char *src = "hello";
     char *dest = my_malloc(strlen(src) + 1);
+    strcpy(dest, src);
+    printf("dest: %s\n", dest);
+
     char *src2 = "goodbye";
     char *dest2 = my_malloc(strlen(src2) + 1);
+    strcpy(dest2, src2);
+    printf("dest2: %s\n", dest2);
+
     char *src3 = "world";
     char *dest3 = my_calloc(1, strlen(src3) + 1);
-    
-    strcpy(dest, src);
-    strcpy(dest2, src2);
     strcpy(dest3, src3);
-    printf("dest: %s\n", dest);
-    printf("dest2: %s\n", dest2);
     printf("dest3: %s\n", dest3);
+
     return 0;
 }
